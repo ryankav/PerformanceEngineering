c3aa03ecda32a2fa02b4524ff717f89e36c9024c
Ryan Kavanagh <ryan.p.kav@gmail.com>
2023-01-16 22:45:06 +0000

    Completing the initial requirements for the project

--- homework1/c-primer/pointer.c
+++ homework1/c-primer/pointer.c
@@ -21,27 +21,27 @@ int main(int argc, char * argv[]) {  // What is the type of argv?
   // pcp is a pointer to a pointer to a char, meaning that
   // pcp stores the address of a char pointer.
   char ** pcp;
-  pcp = argv;  // Why is this assignment valid?
+  pcp = argv;  // Why is this assignment valid? A: The array can decay into a pointer
 
   const char * pcc = c;  // pcc is a pointer to char constant
-  char const * pcc2 = c;  // What is the type of pcc2?
+  char const * pcc2 = c;  // What is the type of pcc2? A: It is also a pointer to char const
 
   // For each of the following, why is the assignment:
-  *pcc = '7';  // invalid?
-  pcc = *pcp;  // valid?
-  pcc = argv[0];  // valid?
+//  *pcc = '7';  // invalid? As the value pointed to is constant
+  pcc = *pcp;  // valid? The pointer itself can be reasigned
+  pcc = argv[0];  // valid? A: Again this decays into a pointer of the first elem.
 
   char * const cp = c;  // cp is a const pointer to char
   // For each of the following, why is the assignment:
-  cp = *pcp;  // invalid?
-  cp = *argv;  // invalid?
-  *cp = '!';  // valid?
+//  cp = *pcp;  // invalid? A: The pointer is constant so can't be reassigned
+//  cp = *argv;  // invalid? A: Same as above
+  *cp = '!';  // valid? A: The value pointed to can be modified
 
   const char * const cpc = c;  // cpc is a const pointer to char const
   // For each of the following, why is the assignment:
-  cpc = *pcp;  // invalid?
-  cpc = argv[0];  // invalid?
-  *cpc = '@';  // invalid?
+//  cpc = *pcp;  // invalid? A: Pointer cannot be reasigned
+//  cpc = argv[0];  // invalid? A: same
+//  *cpc = '@';  // invalid? A: Now the value cannot be modified
 
   return 0;
 }
